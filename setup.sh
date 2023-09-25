f05030136b549d63cfa337447bc4920a71b96b98
IlIllII <78166995+IlIllII@users.noreply.github.com>
2023-09-25 18:55:08 -0400

    Change setup script to only symlink top level

--- setup.sh
+++ setup.sh
@@ -9,58 +9,33 @@ mkdir -p $BACKUP_DIR
 EXCLUDE_FILES=("README.md" "LICENSE" "*.sh" ".gitignore")
 
 should_exclude() {
-  local file_name="$1"
-  for excluded_file in "${EXCLUDE_FILES[@]}"; do
-    [[ "$file_name" == @excluded_file ]] && return 0
-  done
-  return 1
+	local file_name="$1"
+	for excluded_file in "${EXCLUDE_FILES[@]}"; do
+		[[ "$file_name" == @excluded_file ]] && return 0
+	done
+	return 1
 
 }
 
-create_symlink() {
-	local source_path="$1"
-	local target_path="$2"
+for dotfile in $DOTFILES_DIR/.*; do
+	file=$(basename $dotfile)
 
-	if [ -d "$source_path" ]; then
-		mkdir -p "$target_path"
-	else
-		if [ -e "$target_path" ] || [ -L "$target_path" ]; then
-			echo "Moving ~/$target_path to $BACKUP_DIR"
-			mv "$target_path" "$BACKUP_DIR"
-		fi
-    if 
-		echo "Creating symlink for $source_path at $target_path"
-		ln -s "$source_path" "$target_path"
+	if [ "$file" == "." ] || [ "$file" == ".." ]; then
+		continue
 	fi
-}
 
-find "$DOTFILES_DIR" -mindepth 1 -type d -or -type f | while read item; do
-  file_name=$(basename "$item")
+	if should_exclude "$file"; then
+		echo "Skipping $file_name"
+		continue
+	fi
 
-  if should_exclude "$file_name"; then
-    echo "Skipping $file_name"
-    continue
-  fi
+	if [ -e ~/$file ] || [ -L ~/$file ]; then
+		echo "Moving ~/$file to $BACKUP_DIR"
+		mv ~/$file $BACKUP_DIR/
+	fi
 
-	target="${item/$DOTFILES_DIR/~}"
-	create_symlink "$item" "$target"
+	echo "Creating symlink for $file in home directory"
+	ln -s $DOTFILES_DIR/$file ~/$file
 done
 
 echo "...done"
-
-# for dotfile in $DOTFILES_DIR/.*; do
-#   file=$(basename $dotfile)
-#
-#   if [ "$file" == "." ] || [ "$file" == ".." ] || [ "$file" == ".gitignore" ]; then
-#     continue
-#   fi
-#
-#   if [ -e ~/$file ]; then
-#     echo "Moving ~/$file to $BACKUP_DIR"
-#     mv ~/$file $BACKUP_DIR/
-#   fi
-#
-#   echo "Creating symlink for $file in home directory"
-#   ln -s $DOTFILES_DIR/$file ~/$file
-# done
-# echo "...done"
